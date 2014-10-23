e0f272c19e18136a9f05f2d828e562dac64bd1dd
Kent Boogaart <kent.boogaart@gmail.com>
2014-10-23 11:19:54 +1030

    Added sqlstring type, which facilitates relational constraints against string columns. Technically this doesn't need to reside in the SQLite.NET code base, but am putting there for now.

---
+++ src/SQLite.Net/sqlstring.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace SQLite.Net
+{
+    public struct sqlstring : IComparable<sqlstring>, IEquatable<sqlstring>, ISerializable<String>
+    {
+        private readonly string @string;
+
+        public sqlstring(string @string)
+        {
+            this.@string = @string;
+        }
+
+        public static implicit operator string(sqlstring @sqlstring)
+        {
+            return @sqlstring.@string;
+        }
+
+        public static implicit operator sqlstring(string @string)
+        {
+            return new sqlstring(@string);
+        }
+
+        public static bool operator >(sqlstring lhs, sqlstring rhs)
+        {
+            return lhs.CompareTo(rhs) > 0;
+        }
+
+        public static bool operator >=(sqlstring lhs, sqlstring rhs)
+        {
+            return lhs.CompareTo(rhs) >= 0;
+        }
+
+        public static bool operator <(sqlstring lhs, sqlstring rhs)
+        {
+            return lhs.CompareTo(rhs) < 0;
+        }
+
+        public static bool operator <=(sqlstring lhs, sqlstring rhs)
+        {
+            return lhs.CompareTo(rhs) <= 0;
+        }
+
+        public static bool operator ==(sqlstring lhs, sqlstring rhs)
+        {
+            return lhs.CompareTo(rhs) == 0;
+        }
+
+        public static bool operator !=(sqlstring lhs, sqlstring rhs)
+        {
+            return lhs.CompareTo(rhs) != 0;
+        }
+
+        public int CompareTo(sqlstring other)
+        {
+            return string.Compare(this.@string, other.@string, StringComparison.Ordinal);
+        }
+
+        public bool Equals(sqlstring other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is sqlstring))
+            {
+                return false;
+            }
+
+            return this.Equals((sqlstring)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.@string.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.@string;
+        }
+
+        public string Serialize()
+        {
+            return this.@string;
+        }
+    }
+}
